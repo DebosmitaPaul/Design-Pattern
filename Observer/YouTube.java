e964ab96973ffdcbcb2c9a5bf202908844861edf
DebosmitaPaul <debo.paul03@gmail.com>
2021-03-28 11:39:06 +0530

    Create YouTube.java

---
+++ Observer/YouTube.java
@@ -0,0 +1,20 @@
+public class YouTube {
+    public static void main(String[] args) {
+        ChannelOnYouTube channel = new ChannelOnYouTube();
+
+        Subscriber s1 = new Subscriber("Debo");
+        Subscriber s2 = new Subscriber("paul");
+        Subscriber s3 = new Subscriber("Rahul");
+        Subscriber s4 = new Subscriber("Nishant");
+
+        channel.register(s1);
+        channel.register(s2);
+        channel.register(s3);
+        channel.register(s4);
+
+        channel.unRegister(s3);
+
+        channel.uploadVedio("Song..");
+
+    }
+}
